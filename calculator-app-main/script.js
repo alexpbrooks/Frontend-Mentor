834dc6372da772dbba967d4ae7471986454cbf74
Alex <alexpbrooks3@gmail.com>
2022-10-31 14:44:49 +0000

    2022-10-31 14:44

--- calculator-app-main/script.js
+++ calculator-app-main/script.js
@@ -4,7 +4,6 @@ function setTheme(theme) {
 }
 
 function changeTheme() {
-    
     let currentTheme = localStorage.getItem('theme');
     if (currentTheme === 'theme-1') { setTheme('theme-2'); document.querySelector('.dot').style.marginLeft = '28px'; }
     if (currentTheme === 'theme-2') { setTheme('theme-3'); document.querySelector('.dot').style.marginLeft = '50px'; }
@@ -15,3 +14,74 @@ if (localStorage.getItem('theme') === null || document.documentElement.className
     setTheme('theme-1');
 }
 
+document.querySelector('.pos').addEventListener('click', () => changeTheme())
+document.querySelector('#one').addEventListener('click', () => { setTheme('theme-1'); document.querySelector('.dot').style.marginLeft = '5px'; })
+document.querySelector('#two').addEventListener('click', () => { setTheme('theme-2'); document.querySelector('.dot').style.marginLeft = '28px'; })
+document.querySelector('#three').addEventListener('click', () => { setTheme('theme-3'); document.querySelector('.dot').style.marginLeft = '50px'; })
+
+document.querySelector('#equals').addEventListener('click', () => calculateSum())
+document.querySelector('#del').addEventListener('click', () => clearNumber())
+document.querySelector('#reset').addEventListener('click', () => resetCalculator())
+document.querySelector('#dot').addEventListener('click', () => addDecimal())
+
+
+let numberString = ''
+let calculatorList = []
+
+function addNumber(number) {
+    numberString = numberString+number
+    document.querySelector('.output').textContent=numberString
+}
+
+function checkNumberIsFloat(number) {
+    if (typeof number === 'number' && Number.isNaN(number) === false && Number.isInteger(number) === false) {
+        return true
+    }
+    return false
+}
+
+function addOperator(operator) {
+    if (numberString !== '') {
+        calculatorList.push(parseInt(numberString))
+        numberString = ''
+        console.log (calculatorList)
+    }
+    if (typeof(calculatorList[(calculatorList.length-1)]) === 'number') {
+        calculatorList.push(operator)
+        console.log(operator)
+    }
+}
+
+function clearNumber() {
+    numberString = ''
+    document.querySelector('.output').textContent=''
+}
+
+function addDecimal() {
+    if (numberString === '') {numberString = '0'}
+    numberString = numberString+'.'
+    document.querySelector('.output').textContent=numberString
+}
+
+function resetCalculator() {
+    clearNumber();
+    // toMath = []
+}
+
+function calculateSum() {
+    console.log('calc')
+}
+
+for (let number=0; 10>number; number++) {
+    document.getElementById(number).addEventListener('click', () => addNumber(number) )
+}
+
+operators = [
+    {name: 'add', symbol: '+', key: '+'},
+    {name: 'sub', symbol: '-', key: '-'},
+    {name: 'div', symbol: '÷', key: '/'},
+    {name: 'mul', symbol: '×', key: '*'}  
+]
+operators.forEach(operator => {
+    document.getElementById(operator.name).addEventListener('click', () => addOperator(operator.name))
+})
--- github-user-search-app/script.js
+++ github-user-search-app/script.js
@@ -10,11 +10,11 @@ document.querySelector('#togglelight').addEventListener('click', () => {
     document.querySelector('#toggledark').className='';
 })
 
-let url = 'https://api.github.com/users/octocat'
-
-async function pullData(url) {
+async function pullData(username) {
+    let url = 'https://api.github.com/users/'+username
     let response = await fetch(url);
-    let result = await response.json().then(saveData(result));  
+    let result = await response.json();
+    return saveData(result)
 }
 
 function saveData(result) {
@@ -31,9 +31,66 @@ function saveData(result) {
     resultData['name'] = result['name']
     resultData['public_repos'] = result['public_repos']
     resultData['twitter_username'] = result['twitter_username']
-    return resultData
+    return populateData(resultData)
 }
 
-console.log (
-pullData(url)
-)
+function populateUserInfo(resultData) {
+    const userLink = document.querySelector('.userlink')
+    if (resultData['twitter_username'] !== null){
+        userLink.querySelector('#twitter').className = ''
+        userLink.querySelector('#twitter').textContent = resultData['twitter_username']
+    } 
+    else {
+        userLink.querySelector('#twitter').className = 'nolink'
+        userLink.querySelector('#twitter').textContent = 'Not Available'
+    }
+    if (resultData['location'] !== null){
+        userLink.querySelector('#location').className = ''
+        userLink.querySelector('#location').textContent = resultData['location']
+    }
+    else {
+        console.log(resultData['location'])
+        userLink.querySelector('#location').className = 'nolink'
+        userLink.querySelector('#location').textContent = 'Not Available'
+    }
+    if (resultData['blog'] !== ""){
+        userLink.querySelector('#website').className = ''
+        userLink.querySelector('#website').textContent = resultData['blog']
+    }
+    else {
+        userLink.querySelector('#website').className = 'nolink'
+        userLink.querySelector('#website').textContent = 'Not Available'
+    }
+    if (resultData['company'] !== null){
+        userLink.querySelector('#company').className = ''
+        userLink.querySelector('#company').textContent = resultData['company']
+    }
+    else {
+        userLink.querySelector('#company').className = 'nolink'
+        userLink.querySelector('#company').textContent = 'Not Available'
+    }
+}
+
+function populateData(resultData) {
+    document.querySelector('.main').querySelector('img').src = resultData['avatar_url']
+    document.querySelector('.userhead').querySelector('img').src = resultData['avatar_url']
+    const userInfo = document.querySelector('.userinfo')
+    userInfo.querySelector('h2').textContent = resultData['name']
+    userInfo.querySelector('h3').textContent = resultData['login']
+    userInfo.querySelector('h4').textContent = 'Joined '+new Date(resultData['created_at']).toDateString()
+    document.querySelector('.userdesc').textContent = resultData['bio']
+    const userStat = document.querySelector('.userstat')
+    userStat.querySelector('#repos').textContent = resultData['public_repos'] 
+    userStat.querySelector('#followers').textContent = resultData['followers'] 
+    userStat.querySelector('#following').textContent = resultData['following'] 
+    populateUserInfo(resultData)
+}
+
+document.querySelector('button').addEventListener('click',()=> {
+    const searchQuery = document.querySelector('#usersearch').value
+    if(searchQuery != null) {pullData(searchQuery)}
+})
+
+pullData('octocat')
+
+// TODO CSS messes up if .userdesc is empty
